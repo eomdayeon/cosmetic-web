f552681fd2c2d34a4d5869a13a2aa7df6bcd492b
eomdayeon <69031678+eomdayeon@users.noreply.github.com>
2021-11-30 18:23:42 +0900

    nodejs login register main,,db connect

---
+++ public/js/login.js
@@ -0,0 +1,31 @@
+const id = document.querySelector("#id"),
+password = document.querySelector("#password"),
+    loginBtn = document.querySelector("#loginBtn");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.success){
+                location.href="../index.html";
+            } else{
+                alert(res.msg);
+            }
+        })
+            .catch((err) => {
+                console.error(new Error("로그인 중 에러 발생"));
+            });
+}
--- server.js
+++ server.js
@@ -7,44 +7,75 @@ var app = express();
 var server = http.createServer(app);
 var path = require('path');
 
-app.use(express.static(__dirname+'/public'));
+var bodyParser = require('body-parser');
+var config = {
+    user: "hr",
+    password: "hr",
+    connectString: "orcl"
+}
 
-server = app.listen(3000, () => {
-    console.log('server start, port 3000')
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json());
+
+
+app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/html'));
+server = app.listen(4000, () => {
+    console.log('server start, port 4000')
 })
 
 
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT
 
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT
+oracledb.getConnection(config, (err, conn) =>{
+    todoWork(err, conn);
+});
+
 app.get('/', function(request, response) {
     response.sendFile(__dirname+'/index.html');
 })
 
-async function getSelect(request, response) {
-    let connection
-    try {
-        connection = await oracledb.getConnection({
-            user          : "hr",
-            password      : "hr",
-            connectString : "orcl"
-        })
+app.get('/login', function(request, response) {
+    response.sendFile(__dirname+'/html/login.html');
+})
 
-        const result = await connection.execute(
-            `SELECT * 
-            FROM COSMETICS`,
-        )
+app.get('/register', function(request, response) {
+    response.sendFile(__dirname+'/html/register.html');
+})
 
-        console.log(result)
-        response.send(result.rows)
-    } catch (error) {
-        console.log(error)
-    } finally {
-        if (connection) {
-            try {
-                await connection.close()
-            } catch (error) {
-                console.log(error)
-            }
-        }
+app.post("/login", async(req,res)=> {
+    const user = new User(req.body);
+    const response = await user.login();
+    return res.json(response);
+});
+
+
+function todoWork(err, connection) {
+    if (err) {
+        console.error(err.message);
+        return;
     }
+    connection.execute("select * from cosmetics", [], function (err, result) {
+        if (err) {
+            console.error(err.message);
+            doRelease(connection);
+            return;
+        }
+        console.log(result.metaData);  //테이블 스키마
+        console.log(result.rows);  //데이터
+        doRelease(connection);
+    });
+
+    
+}  
+
+function doRelease(connection) {
+    connection.release(function (err) {
+        if (err) {
+            console.error(err.message);
+        }
+    });
 }
